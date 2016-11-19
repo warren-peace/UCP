dffd5a2b1e1442b541599ce138875b1b03008891
warren-peace <screw.weee@gmail.com>
2016-11-20 01:53:53 +0800

    initial commit

---
+++ js/automata_framework.js
@@ -0,0 +1,143 @@
+function Node(data) {
+  this.data = data;
+  this.parent = null;
+  this.children = [];
+}
+
+function Tree(data) {
+    var node = new Node(data);
+    this._root = node;
+}
+
+var tree = new Tree('N');
+
+// depth-first traversal
+// traverseDF(callback)
+Tree.prototype.traverseDF = function(callback) {
+ 
+    // this is a recurse and immediately invoking function 
+    (function recurse(currentNode) {
+        for (var i = 0, length = currentNode.children.length; i < length; i++) {
+            recurse(currentNode.children[i]);
+        }
+        callback(currentNode);
+    })(this._root);
+ 
+};
+
+// breadth-first search
+// traverseBF(callback)
+Tree.prototype.traverseBF = function(callback) {
+    var queue = new Queue();
+     
+    queue.enqueue(this._root);
+ 
+    currentTree = queue.dequeue();
+ 
+    while(currentTree){
+        for (var i = 0, length = currentTree.children.length; i < length; i++) {
+            queue.enqueue(currentTree.children[i]);
+        }
+ 
+        callback(currentTree);
+        currentTree = queue.dequeue();
+    }
+};
+
+// contains(callback, traversal)
+Tree.prototype.contains = function(callback, traversal) {
+    traversal.call(this, callback);
+};
+
+// add(data, toData, traversal)
+Tree.prototype.add = function(data, toData, traversal) {
+    var child = new Node(data),
+        parent = null,
+        callback = function(node) {
+            if (node.data === toData) {
+                parent = node;
+            }
+        };
+ 
+    this.contains(callback, traversal);
+ 
+    if (parent) {
+        parent.children.push(child);
+        child.parent = parent;
+    } else {
+        throw new Error('Cannot add node to a non-existent parent.');
+    }
+};
+
+// remove(data, fromData, traversal)
+Tree.prototype.remove = function(data, fromData, traversal) {
+    var tree = this,
+        parent = null,
+        childToRemove = null,
+        index;
+ 
+    var callback = function(node) {
+        if (node.data === fromData) {
+            parent = node;
+        }
+    };
+ 
+    this.contains(callback, traversal);
+ 
+    if (parent) {
+        index = findIndex(parent.children, data);
+ 
+        if (index === undefined) {
+            throw new Error('Node to remove does not exist.');
+        } else {
+            childToRemove = parent.children.splice(index, 1);
+        }
+    } else {
+        throw new Error('Parent does not exist.');
+    }
+ 
+    return childToRemove;
+};
+
+function findIndex(arr, data) {
+    var index;
+ 
+    for (var i = 0; i < arr.length; i++) {
+        if (arr[i].data === data) {
+            index = i;
+        }
+    }
+ 
+    return index;
+}
+
+// queue
+
+function Queue() {
+    this._oldestIndex = 1;
+    this._newestIndex = 1;
+    this._storage = {};
+}
+ 
+Queue.prototype.size = function() {
+    return this._newestIndex - this._oldestIndex;
+};
+ 
+Queue.prototype.enqueue = function(data) {
+    this._storage[this._newestIndex] = data;
+    this._newestIndex++;
+};
+ 
+Queue.prototype.dequeue = function() {
+    var oldestIndex = this._oldestIndex,
+        newestIndex = this._newestIndex,
+        deletedData;
+ 
+    if (oldestIndex !== newestIndex) {
+        deletedData = this._storage[oldestIndex];
+        delete this._storage[oldestIndex];
+        this._oldestIndex++;
+ 
+        return deletedData;
+    }
+};
