464f90428d87d09e265164d58a5e50c3c2030fa7
warren-peace <screw.weee@gmail.com>
2016-11-22 12:49:44 +0800

    modified tree to have multiple parents

--- js/automata_framework.js
+++ js/automata_framework.js
@@ -1,7 +1,8 @@
 function Node(data) {
-  this.data = data;
-  this.parent = null;
-  this.children = [];
+    this.data = data;
+    this.id = 0;
+    this.parent = [];
+    this.children = [];
 }
 
 function Tree(data) {
@@ -9,7 +10,9 @@ function Tree(data) {
     this._root = node;
 }
 
-var tree = new Tree('N');
+// root
+var tree = new Tree('R');
+
 
 // depth-first traversal
 // traverseDF(callback)
@@ -30,9 +33,7 @@ Tree.prototype.traverseDF = function(callback) {
 Tree.prototype.traverseBF = function(callback) {
     var queue = new Queue();
      
-    queue.enqueue(this._root);
- 
-    currentTree = queue.dequeue();
+    currentTree = this._root;
  
     while(currentTree){
         for (var i = 0, length = currentTree.children.length; i < length; i++) {
@@ -49,12 +50,12 @@ Tree.prototype.contains = function(callback, traversal) {
     traversal.call(this, callback);
 };
 
-// add(data, toData, traversal)
-Tree.prototype.add = function(data, toData, traversal) {
+// add(data, toData, toId, traversal)
+Tree.prototype.add = function(data, id, toData, toId, traversal) {
     var child = new Node(data),
         parent = null,
         callback = function(node) {
-            if (node.data === toData) {
+            if (node.data === toData && node.id === toId) {
                 parent = node;
             }
         };
@@ -62,22 +63,82 @@ Tree.prototype.add = function(data, toData, traversal) {
     this.contains(callback, traversal);
  
     if (parent) {
+        child.id = id;
         parent.children.push(child);
-        child.parent = parent;
+        child.parent.push(parent);
     } else {
         throw new Error('Cannot add node to a non-existent parent.');
     }
 };
 
-// remove(data, fromData, traversal)
-Tree.prototype.remove = function(data, fromData, traversal) {
+// connectNodes(cData, cId, pData, pId, traversal)
+Tree.prototype.connectNodes = function(cData, cId, pData, pId, traversal) {
+    var parent = null,
+        child = null;
+        
+    // find parent    
+    var callback = function(node) {
+        if (node.data === pData && node.id === pId) {
+            parent = node;
+        }
+    }
+    this.contains(callback, traversal);
+    
+    // find child
+    callback = function(node) {
+        if (node.data === cData && node.id === cId) {
+            child = node;
+        }
+    }
+    this.contains(callback, traversal);
+    
+    if(parent){
+        if(child){
+            parent.children.push(child);
+            child.parent.push(parent);
+        } else {
+            throw new Error('Cannot connect node to a non-existent child.');
+        }
+    } else {
+        throw new Error('Cannot connect node to a non-existent parent.');
+    }
+};
+
+// removeParent(cData, cId, pData, pId, traversal)
+Tree.prototype.removeParent = function(cData, cId, pData, pId, traversal){
+    var child = null,
+        parentToRemove = null;
+    
+    // find child
+    var callback = function(node) {
+        if (node.data === cData && node.id === cId) {
+            child = node;
+        }
+    }
+    this.contains(callback, traversal);
+    
+    if(child){
+        index = findIndex(child.parent,pData,pId);
+        if (index === undefined) {
+            throw new Error('Node to remove does not exist.');
+        } else {
+            parentToRemove = child.parent.splice(index, 1);
+        }
+    } else {
+        throw new Error('Child does not exist.');
+    }
+    return parentToRemove;
+};
+
+// remove(data, id, fromData, fromId, traversal)
+Tree.prototype.remove = function(data, id, fromData, fromId, traversal) {
     var tree = this,
         parent = null,
         childToRemove = null,
         index;
  
     var callback = function(node) {
-        if (node.data === fromData) {
+        if (node.data === fromData && node.id === fromId) {
             parent = node;
         }
     };
@@ -85,25 +146,27 @@ Tree.prototype.remove = function(data, fromData, traversal) {
     this.contains(callback, traversal);
  
     if (parent) {
-        index = findIndex(parent.children, data);
+        index = findIndex(parent.children, data, id);
  
         if (index === undefined) {
             throw new Error('Node to remove does not exist.');
         } else {
             childToRemove = parent.children.splice(index, 1);
+            
         }
     } else {
         throw new Error('Parent does not exist.');
     }
- 
+    
+    
     return childToRemove;
 };
 
-function findIndex(arr, data) {
+function findIndex(arr, data, id) {
     var index;
  
     for (var i = 0; i < arr.length; i++) {
-        if (arr[i].data === data) {
+        if (arr[i].data === data && arr[i].id === id) {
             index = i;
         }
     }
@@ -141,3 +204,13 @@ Queue.prototype.dequeue = function() {
         return deletedData;
     }
 };
+
+//test
+// tree.add('A', 0, 'R', 0, tree.traverseBF);
+// tree.add('B', 0, 'A', 0, tree.traverseBF);
+// tree.add('B', 1, 'A', 0, tree.traverseBF);
+// console.log(tree._root.children[0]);
+// tree.connectNodes('B', 0, 'R', 0, tree.traverseBF);
+// var a = tree.remove('A', 0, 'R', 0, tree.traverseBF);
+// var b = tree.removeParent('B', 0, 'A', 0, tree.traverseBF);
+// console.log(tree._root.children);
